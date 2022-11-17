0ff5399a720b213fafdd2b82bfac0ca007a4dbc5
cjb1n <86945783+cjb1n@users.noreply.github.com>
2022-11-18 01:24:31 +0900

    Create route.js

---
+++ route.js
@@ -0,0 +1,37 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+const routes =  [
+  {
+    path: "/",
+    name: "main",
+    component: () => import("./components/main")
+   },
+  {
+    path: "/memberlist",
+    alias: "/memberlist",
+    name: "memberlist",
+    component: () => import("./components/Memberlist")
+  },
+  {
+    path: "/tutorials/:id",
+    name: "tutorial-details",
+    component: () => import("./components/Tutorial")
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: () => import("./components/Signup")
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("./components/login")
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
